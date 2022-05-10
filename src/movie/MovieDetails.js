4305e1b4880cef698887c340783a4cfd3e5f817e
Siva KarthickRajan <sivakarthickrajan@gmail.com>
2022-05-11 00:41:17 +0530

    Onclick a moviecard shows MovieDuration --added

--- src/movie/MovieDetails.js
+++ src/movie/MovieDetails.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "../moviedetails.css";
 const MovieDetails = ({ moviedetails }) => {
-  console.log("backdrop_path", moviedetails.backdrop_path);
+  const hrs = Math.floor(moviedetails.runtime / 60);
+  const mins = moviedetails.runtime % 60;
+
   return (
-    <div className="aboutMovie">
-      <div>
-        <img
-          className="detailImage"
-          src={`https://image.tmdb.org/t/p/original/${moviedetails.backdrop_path}`}
-          alt=""
-        ></img>
+    <>
+      <img
+        className="bg_image"
+        src={`https://image.tmdb.org/t/p/original/${moviedetails.backdrop_path}`}
+        alt=""
+      ></img>
+      <div className="aboutMovie">
+        <div className="aboutMovie_left">
+          <img
+            className="thumbnail_image"
+            src={`https://image.tmdb.org/t/p/w300/${moviedetails.poster_path}`}
+            alt=""
+          ></img>
+        </div>
 
         <div className="aboutMovie_right">
-          <div className="aboutmovie_title">
-            <h1>{moviedetails.original_title}</h1>
-            <h2>{moviedetails.tagline}</h2>
+          <h1 style={{ fontFamily: "Josefin Sans" }}>
+            {moviedetails.original_title}
+          </h1>
+          <div className="aboutMovie_runtime">
+            <h6 style={{ fontStyle: "italic" }}>
+              {hrs}Hrs {mins}Mins
+            </h6>
+            <h5 style={{ display: "flex", gap: "10px" }}>
+              {moviedetails.genres?.length > 0 &&
+                moviedetails.genres.map((genre) => {
+                  return <div>{genre.name},</div>;
+                })}
+            </h5>
           </div>
-          <div className="aboutmovie_details">
-            <div>{moviedetails.overview}</div>
-            <div>Budget : $ {moviedetails.budget}</div>
-            {/* <div>Category : {moviedetails?.belongs_to_collection}</div> */}
-          </div>
+
+          <h2>{moviedetails.tagline}</h2>
+
+          <div>{moviedetails.overview}</div>
+          <div>Budget : $ {moviedetails.budget}</div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default MovieDetails;
